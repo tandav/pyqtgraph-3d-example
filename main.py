45106b85f8bf7ace8a34aa652b1190b3685a0f6b
Alexander Rodionov <tandav@tandav.me>
2022-04-14 23:02:32 +0300

    [WIP] backup

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from PyQt6.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import sys
+import pickle
+
+
+with open('graph.pkl', 'rb') as f:
+    graph = pickle.load(f)
 
 
 class Window(QDialog):
@@ -43,25 +48,46 @@ class Window(QDialog):
         self.main_scatter_plot = gl.GLScatterPlotItem()
         self.color = (1, 0.7, 0.4, 1)
 
-        x = pd.read_csv('X.csv', index_col=0)
-        # x = np.load('X.npy')
-        # x = np.random.random((1000, 3))
-        # x = x * 10
+        X = pd.read_csv('X.csv', index_col=0)
+        tup = tuple(X.index)
+        # X = np.load('X.npy')
+        # X = np.random.random((1000, 3))
+        # X = X * 10
         # print(x.shape)
 
-        # ind = np.random.choice(x.shape[0], size=40_000, replace=False)
-        # x = x[ind]
-        self.main_scatter_plot.setData(pos=x.values, size=0.2, color=self.color, pxMode=False)
+        # ind = np.random.choice(X.shape[0], size=40_000, replace=False)
+        # X = X[ind]
+        self.main_scatter_plot.setData(pos=X.values, size=0.2, color=self.color, pxMode=False)
         self.w.addItem(self.main_scatter_plot)
 
         # txtitem2 = gl.GLTextItem()
         # txtitem2.setData(pos=(1.0, -1.0, 2.0), color=(127, 255, 127, 255), text='text2')
         # self.w.addItem(txtitem2)
 
-        for row in x.itertuples():
+        for row in X.itertuples():
             t = gl.GLTextItem()
             t.setData(pos=(row.x, row.y, row.z), color=(127, 255, 127, 255), text=row.Index)
             self.w.addItem(t)
+
+
+        for k, v in graph.items():
+            for vv in v:
+                pts = np.linspace(X.loc[k].values, X.loc[vv].values)
+                p = gl.GLLinePlotItem(pos=pts, color=(1, 1, 1, 0.3), width=1., antialias=True)
+                self.w.addItem(p)
+
+            # i = tup.index(k)
+            # js = [tup.index(vv) for vv in v]
+
+            # for j in js:
+            #
+            #     pts = np.linspace(x.loc[k], [row.x, row.y, row.z])
+            #     plt.plot([e[i, 0], e[j, 0]], [e[i, 1], e[j, 1]])
+
+
+        # pts = np.linspace([0, 0, 0], [row.x, row.y, row.z])
+        # p = gl.GLLinePlotItem(pos=pts, width=1., antialias=True)
+        # self.w.addItem(p)
 
         layout = QVBoxLayout()
         layout.addWidget(self.w)
