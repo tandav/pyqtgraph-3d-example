a92a5ae24cc3b44bb88c1c38169c488cb9775693
Alexander Rodionov <tandav@tandav.me>
2023-02-05 14:28:00 +0400

    pre-commit

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,9 +1,9 @@
+import os
 import subprocess
 import sys
-import os
 from unittest import mock
 
 
 def test_smoke():
     with mock.patch.dict(os.environ, {'TEST': 'true'}):
-        subprocess.check_call([sys.executable, "main.py"])
+        subprocess.check_call([sys.executable, 'main.py'])
